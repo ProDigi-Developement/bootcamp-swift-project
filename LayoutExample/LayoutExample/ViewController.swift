4de531b7587dc41de11d00580aeaf0795bcf7479
Caio Dias <kiobreno@gmail.com>
2017-12-08 12:25:15 -0500

    Add LayoutExample project

---
+++ LayoutExample/LayoutExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutExample
+//
+//  Created by Caio Dias on 2017-12-08.
+//  Copyright © 2017 ProDigi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
