5fae8b1f887adf4b7696b8b5c7ccb2fa3360f87e
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2017-12-15 14:16:46 -0500

    Update storyboard with view controller

---
+++ Course Project/Course Project/AraceliProject/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Course Project
+//
+//  Created by Araceli Teixeira on 15/12/17.
+//  Copyright © 2017 ProDigi. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+}
