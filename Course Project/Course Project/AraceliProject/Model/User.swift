e1d73ce7b2b8e100792cd5a988f13bab05a28425
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2017-12-15 15:31:26 -0500

    Create class to hold users retrieved data

--- Course Project/Course Project/AraceliProject/Model/User.swift
+++ Course Project/Course Project/AraceliProject/Model/User.swift
@@ -6,8 +6,27 @@
 //  Copyright © 2017 ProDigi. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-class User {
+class User: Person {
+    public var address: String
+    public var city: String
+    public var state: String
+    public var postcode: String
+    public var country: String
+    public var pictureURL: String
+    public var picture: UIImage
     
+    init(_ firstName: String, _ lastName: String, _ email: String, _ address: String, _ city: String,
+         _ state: String, _ postcode: String, _ country: String, _ pictureURL: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.address = address
+        self.city = city
+        self.state = state
+        self.postcode = postcode
+        self.country = country
+        self.pictureURL = pictureURL
+    }
 }
