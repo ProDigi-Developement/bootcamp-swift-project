0b70fc8782fb0b831b126737a29724b4c4e56c4b
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2017-12-15 12:18:23 -0500

    Create view controller for my main page

---
+++ Course Project/Course Project/AraceliProject/View/MasterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MasterViewController.swift
+//  Course Project
+//
+//  Created by Araceli Teixeira on 15/12/17.
+//  Copyright © 2017 ProDigi. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
