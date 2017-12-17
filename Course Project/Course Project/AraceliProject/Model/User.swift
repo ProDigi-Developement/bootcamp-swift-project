f1327cb5d7eb3e3151cb152e056fa7e6af483d05
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2017-12-17 11:19:29 -0500

    Create extension to manage image downloads

--- Course Project/Course Project/AraceliProject/Model/User.swift
+++ Course Project/Course Project/AraceliProject/Model/User.swift
@@ -16,8 +16,6 @@ class User: Person {
     public let country: String
     public let pictureURL: String
     public let iconURL: String
-    public var picture: UIImage? = nil
-    public var icon: UIImage? = nil
     
     init(_ firstName: String, _ lastName: String, _ email: String, _ address: String, _ city: String,
          _ state: String, _ postcode: String, _ country: String, _ pictureURL: String, _ iconURL: String) {
@@ -30,4 +28,47 @@ class User: Person {
         self.iconURL = iconURL
         super.init(firstName: firstName, lastName: lastName, email: email)
     }
+}
+
+extension User {
+    
+    public var picture: UIImage? {
+        return loadImage(imageUrl: pictureURL)
+    }
+    public var icon: UIImage? {
+        return loadImage(imageUrl: iconURL)
+    }
+
+    private func loadImage(imageUrl: String) -> UIImage? {
+        guard let url = URL(string: imageUrl) else {
+            print("Error: Not possible to create the URL object")
+            return nil
+        }
+        var image: UIImage?
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        (session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error)")
+            } else {
+                guard let httpResponse = response as? HTTPURLResponse,
+                    httpResponse.statusCode == 200
+                    else {
+                        print("Error: Error on fetch.")
+                        return
+                }
+                
+                if let data = data {
+                    image = UIImage(data: data)
+                } else {
+                    print("Error: Data is null")
+                }
+            }
+        }).resume()
+        
+        return image
+    }
+    
+    
 }
