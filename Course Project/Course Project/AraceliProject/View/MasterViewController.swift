378112b22c425596af42df2535733ce1c37c82a1
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2017-12-17 16:46:52 -0500

    Prepare view controller to receive data

--- Course Project/Course Project/AraceliProject/View/MasterViewController.swift
+++ Course Project/Course Project/AraceliProject/View/MasterViewController.swift
@@ -11,29 +11,18 @@ import MapKit
 
 class MasterViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
-    var list: [User] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         mapView.delegate = self
         
-        print("Fetching user list...")
         UserController.sharedInstance.fetchUserList(onSuccess: onSuccessScenario, onFail: onFailScenario)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     private func onSuccessScenario() {
         DispatchQueue.main.async {
-            self.list = UserController.sharedInstance.userList
-            for user in self.list {
-                print(user.fullName())
-            }
+            self.placeUsers(UserController.sharedInstance.userList)
         }
     }
     
@@ -41,16 +30,9 @@ class MasterViewController: UIViewController {
         print(errorMessage)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func placeUsers(_ list:[User]) {
+        
     }
-    */
 }
 
 extension MasterViewController: MKMapViewDelegate {
