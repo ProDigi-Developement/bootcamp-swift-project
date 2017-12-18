bb0ae13736c41294fb4e4d06b16f53806448b771
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2017-12-18 15:23:32 -0500

    Refine coordinates

--- Course Project/Course Project/AraceliProject/Controller/RandomLocationGenerator.swift
+++ Course Project/Course Project/AraceliProject/Controller/RandomLocationGenerator.swift
@@ -9,20 +9,20 @@
 import Foundation
 
 class RandomLocationGenerator {
-    private static let minLat = 43.5
+    private static let minLat = 43.6
     private static let maxLat = 43.8
-    private static let minLong = -79.9
-    private static let maxLong = -79.0
+    private static let minLong = -79.7
+    private static let maxLong = -79.2
     private static let scale = 100000.0
     
     public static func generateCoordinates() -> (Double, Double) {
         var lat = minLat + Double(arc4random_uniform(UInt32(scale)))/scale * (maxLat - minLat)
-        var long = minLong + Double(arc4random_uniform(UInt32(scale)))/scale * (maxLong - minLong)
+        let long = minLong + Double(arc4random_uniform(UInt32(scale)))/scale * (maxLong - minLong)
         if long > -79.5 {
-            lat += 0.2
+            lat += 0.1
         }
-        if lat < 43.6 {
-            long -= 0.1
+        if long < -79.6 {
+            lat -= 0.1
         }
         return (lat, long)
     }
