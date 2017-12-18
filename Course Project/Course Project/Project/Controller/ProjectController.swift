e498b584a1b0365f667be6aa523391bd9d6d8893
Alfredo Fernandes <alfredorfernandes@gmail.com>
2017-12-18 18:10:14 -0500

    Update to collect new information from api: street, city, state, postcode, cell

--- Course Project/Course Project/Project/Controller/ProjectController.swift
+++ Course Project/Course Project/Project/Controller/ProjectController.swift
@@ -15,7 +15,7 @@ final class ProjectController {
     public private(set) var list: [Project]
     public static let sharedInstance = ProjectController()
     public var selectedUser: Project? = nil
-    private let url: String = "https://randomuser.me/api/?results=25&seed=alfredo"
+    private let url: String = "https://randomuser.me/api/?results=30&seed=alfredo&nat=us,ca,br"
     
     private init() {
         self.list = [Project]()
@@ -63,17 +63,23 @@ final class ProjectController {
         if let results = json["results"] as? [[String:Any]] {
             for jsonPerson in results {
                 guard let email = jsonPerson["email"] as? String,
-                    let names = jsonPerson["name"] as? [String:Any],
-                    let firstName = names["first"] as? String,
-                    let lastName = names["last"] as? String,
-                    let pictures = jsonPerson["picture"] as? [String:Any],
-                    let imageLarge = pictures["large"] as? String
+                    let names = jsonPerson["name"] as? [String: Any],
+                        let firstName = names["first"] as? String,
+                        let lastName = names["last"] as? String,
+                    let locations = jsonPerson["location"] as? [String: Any],
+                        let street = locations["street"] as? String,
+                        let city = locations["city"] as? String,
+                        let state = locations["state"] as? String,
+                    let postcode = locations["postcode"] as? Int,
+                    let cell = jsonPerson["cell"] as? String,
+                    let pictures = jsonPerson["picture"] as? [String: Any],
+                        let imageLarge = pictures["large"] as? String
                     else {
                         print("Not possible to find the email and first name.")
                         break
                 }
                 
-                tempList.append(Project(firstName: firstName, lastName: lastName, email: email, pictureUrl: imageLarge))
+                tempList.append(Project(firstName: firstName, lastName: lastName, street: street, city: city, state: state, postcode: postcode, email: email, cell: cell, pictureUrl: imageLarge))
             }
         } else {
             print("No results tag found in response JSON.")
